d33f694887529a70052db878eec1f99ba6d78804
7alip <talipaltas@gmail.com>
2022-02-27 15:58:57 +0100

    Fix auth condition

--- src/components/auth/require-auth.tsx
+++ src/components/auth/require-auth.tsx
@@ -19,7 +19,7 @@ export const RequireAuth = ({ children }: { children: JSX.Element }) => {
     dispatch(checkAuth())
   }, [])
 
-  if (!parsedItem?.token || !token) {
+  if (!parsedItem?.token && !token) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
